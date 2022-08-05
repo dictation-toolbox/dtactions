0f35b9b38619430e4d98941e995c37b8cf1ede44
LexiconCode <CasterVoice@protonmail.com>
2022-08-05 12:10:08 -0500

    use py launcher

--- build_package.ps1
+++ build_package.ps1
@@ -1,4 +1,4 @@
 #build an sdist (source distribution) package for unimacro
 #no wheel is built, one less thing to worry about
 #use the no-setup option, which requires a 
-flit build --format sdist  --no-setup-py
+py -m flit build --format sdist  --no-setup-py
--- publish_package_pypi.ps1
+++ publish_package_pypi.ps1
@@ -1,4 +1,4 @@
 #build an sdist (source distribution) package for unimacro
 #no wheel is built, one less thing to worry about
 #use the no-setup option, which requires a 
-flit publish  --format sdist  --no-setup-py  --repository pypi
+py -m flit publish  --format sdist  --no-setup-py  --repository pypi
