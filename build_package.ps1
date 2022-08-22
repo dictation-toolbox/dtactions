0469433ad9b854ed8096758db8a6a809193eac0b
Doug Ransom <doug.ransom@gmail.com>
2022-08-22 12:23:37 -0700

    builds wheel and sdist.

--- build_package.ps1
+++ build_package.ps1
@@ -1,4 +1,2 @@
-#build an sdist (source distribution) package for unimacro
-#no wheel is built, one less thing to worry about
-#use the no-setup option, which requires a 
-py -m flit build --format sdist  --no-setup-py
+
+flit build 
